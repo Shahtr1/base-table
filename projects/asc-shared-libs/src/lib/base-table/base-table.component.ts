572c75fcdece09a7488d11c697318128f5e0cfa4
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2023-07-31 07:47:16 +0530

    added client side pagination

--- projects/asc-shared-libs/src/lib/base-table/base-table.component.ts
+++ projects/asc-shared-libs/src/lib/base-table/base-table.component.ts
@@ -7,8 +7,29 @@ import { Size } from '../model/base-table.model';
   styleUrls: ['./base-table.component.css'],
 })
 export class BaseTableComponent {
+  private _rowSize = 10;
+
+  @Input() set rowSize(value: number) {
+    this._rowSize = value;
+    this.rowsPerPageOptions.find((size) => size === value) ||
+      this.rowsPerPageOptions.push(value);
+  }
+
+  get rowSize(): number {
+    return this._rowSize;
+  }
+
+  @Input() paginator = true;
+
+  @Input() responsiveLayoutBreakPoint: number = 768;
+
+  @Input() currentPageReportTemplate =
+    'Showing {first} to {last} of {totalRecords} entries';
+
+  @Input() rowsPerPageOptions = [10, 20, 50];
+
   @Input() set size(value: Size) {
-    this.selectedSize = this.sizes.find((size) => size.name === value)!;
+    this._selectedSize = this.sizes.find((size) => size.name === value)!;
   }
 
   private sizes: { name: Size; class: string }[] = [
@@ -18,16 +39,22 @@ export class BaseTableComponent {
   ];
 
   // set default size to normal
-  selectedSize: { name: Size; class: string } = this.sizes[1];
+  private _selectedSize: { name: Size; class: string } = this.sizes[1];
+
+  get selectedSize(): { name: Size; class: string } {
+    return this._selectedSize;
+  }
 
   products: { code: string; category: string }[] = [
-    {
-      code: '1',
-      category: 'New',
-    },
-    {
-      code: '2',
-      category: 'Old',
-    },
+    { code: '1', category: 'New' },
+    { code: '2', category: 'Old' },
+    { code: '3', category: 'New' },
+    { code: '4', category: 'Old' },
+    { code: '5', category: 'New' },
+    { code: '6', category: 'Old' },
+    { code: '7', category: 'New' },
+    { code: '8', category: 'Old' },
+    { code: '9', category: 'New' },
+    { code: '10', category: 'Old' },
   ];
 }
