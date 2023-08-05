76151ec207fd919eae7befcfe4f2a44d79a99ede
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2023-08-05 14:30:43 +0530

    Additions

--- projects/asc-shared-libs/src/lib/services/text/fake-text.service.ts
+++ projects/asc-shared-libs/src/lib/services/text/fake-text.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ItextService } from './itext.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { GeneralText } from '../../model/lib.model';
 
 @Injectable({
@@ -9,5 +9,10 @@ import { GeneralText } from '../../model/lib.model';
 export class FakeTextService implements ItextService {
   constructor() {}
 
-  convert(data: GeneralText[]): Observable<GeneralText[]> {}
+  convert(data: GeneralText[]): Observable<GeneralText[]> {
+    return of([
+      { label: 'Code', labelId: 'CODE' },
+      { label: 'Category', labelId: 'CATEGORY' },
+    ]);
+  }
 }
