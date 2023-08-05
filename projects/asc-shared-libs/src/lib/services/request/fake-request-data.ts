a8b21131a9bf35d1ce33ea8665678d9f3425e2ca
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2023-08-06 04:59:15 +0530

    Clean up

---
+++ projects/asc-shared-libs/src/lib/services/request/fake-request-data.ts
@@ -0,0 +1,58 @@
+import { TableViewConfig } from '../../model/table-config.model';
+import * as _ from 'lodash';
+
+export const testBaseTableData: TableViewConfig = {
+  settings: {
+    url: '/api/v5/account-purposes',
+    export: false,
+    editButton: true,
+    deleteButton: true,
+    addButton: true,
+    title: 'L_ACCOUNT_PURPOSE',
+    transformData: true,
+    editCallBack: true,
+    addCallBack: true,
+    softDelete: true,
+    modifyConfig: true,
+  },
+  columns: [
+    { field: 'uuid', headerId: 'L_REF_ID', sort: true, filter: 'search' },
+    {
+      field: 'shortName',
+      headerId: 'L_ACCOUNT_PURPOSE_SHORT_NAME',
+      sort: false,
+      globalSearch: true,
+      filter: 'search',
+    },
+    {
+      field: 'fullName',
+      headerId: 'L_ACCOUNT_PURPOSE_FULL_NAME',
+      filter: 'search',
+    },
+    {
+      field: 'workFlowStates',
+      headerId: 'L_TRXN_STATUS',
+      globalSearch: true,
+      sourceUrl: '/api/v5/work-flow-states?size=20',
+      optionLabel: 'desc',
+      filter: 'select',
+      sort: true,
+      type: 'select',
+      translate: true,
+    },
+  ],
+};
+
+export function getAccountPurposeData() {
+  let testAccountPurposeData = [];
+  for (let i = 0; i < 100; i++) {
+    testAccountPurposeData.push({
+      uuid: 'uuid' + i,
+      shortName: 'shortName' + i,
+      fullName: 'fullName' + i,
+      trxStatus: _.random(0, 1) === 1 ? 'ACTIVE' : 'INACTIVE',
+    });
+  }
+
+  return testAccountPurposeData;
+}
--- projects/asc-shared-libs/src/lib/services/request/fake-request.service.ts
+++ projects/asc-shared-libs/src/lib/services/request/fake-request.service.ts
@@ -1,72 +1,14 @@
 import { Injectable } from '@angular/core';
 import { IRequestService } from './irequest.service';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpVerbs } from '../../model/lib.model';
-import { TableViewConfig } from '../../model/table-config.model';
-import * as _ from 'lodash';
+import { getAccountPurposeData, testBaseTableData } from './fake-request-data';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FakeRequestService implements IRequestService {
-  private testBaseTableData: TableViewConfig = {
-    settings: {
-      url: '/api/v5/account-purposes',
-      export: false,
-      editButton: true,
-      deleteButton: true,
-      addButton: true,
-      title: 'L_ACCOUNT_PURPOSE',
-      transformData: true,
-      editCallBack: true,
-      addCallBack: true,
-      softDelete: true,
-      modifyConfig: true,
-    },
-    columns: [
-      { field: 'uuid', headerId: 'L_REF_ID', sort: true, filter: 'search' },
-      {
-        field: 'shortName',
-        headerId: 'L_ACCOUNT_PURPOSE_SHORT_NAME',
-        sort: false,
-        globalSearch: true,
-        filter: 'search',
-      },
-      {
-        field: 'fullName',
-        headerId: 'L_ACCOUNT_PURPOSE_FULL_NAME',
-        filter: 'search',
-      },
-      {
-        field: 'workFlowStates',
-        headerId: 'L_TRXN_STATUS',
-        globalSearch: true,
-        sourceUrl: '/api/v5/work-flow-states?size=20',
-        optionLabel: 'desc',
-        filter: 'select',
-        sort: true,
-        type: 'select',
-        translate: true,
-      },
-    ],
-  };
-
-  private testAccountPurposeData: any[] = [];
-
-  constructor() {
-    this.createAccountPurposeData();
-  }
-
-  private createAccountPurposeData() {
-    for (let i = 0; i < 100; i++) {
-      this.testAccountPurposeData.push({
-        uuid: 'uuid' + i,
-        shortName: 'shortName' + i,
-        fullName: 'fullName' + i,
-        trxStatus: _.random(0, 1) === 1 ? 'ACTIVE' : 'INACTIVE',
-      });
-    }
-  }
+  private testAccountPurposeData = getAccountPurposeData();
 
   request(
     apiEndpoint: string,
@@ -78,7 +20,7 @@ export class FakeRequestService implements IRequestService {
     this.logData(apiEndpoint, method, route, data, options);
     let respData: any;
     if (route.includes('app-table-designs')) {
-      respData = [{ definition: JSON.stringify(this.testBaseTableData) }];
+      respData = [{ definition: JSON.stringify(testBaseTableData) }];
     } else if (route.includes('account-purpose')) {
       respData = this.testAccountPurposeData;
     }
