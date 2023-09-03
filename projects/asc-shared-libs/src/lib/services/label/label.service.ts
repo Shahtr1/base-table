02584f68a96bd2d57382ff9eef9378398f943096
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2023-09-03 23:43:26 +0530

    Added to dos

--- projects/asc-shared-libs/src/lib/services/label/label.service.ts
+++ projects/asc-shared-libs/src/lib/services/label/label.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
 import { ILabelService } from './ilabel.service';
-import { Observable, of } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 import { LabelConfig } from '../../model/label-config.model';
+import { RequestService } from '../request/request.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LabelService implements ILabelService {
+  constructor(private restService: RequestService) {}
+
   getLabels(): Observable<LabelConfig> {
-    return of({});
+    return this.restService.request('GET', '/api/app-labels').pipe(
+      map(({ body }) => {
+        // TODO: get properties from store
+        console.log('body', body);
+        return body;
+      })
+    );
   }
 }
