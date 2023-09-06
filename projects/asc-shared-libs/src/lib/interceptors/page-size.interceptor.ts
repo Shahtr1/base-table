aa39ffbd58021ccbd1b472e5261191639cc72e3e
Shahrukh Tramboo <tramboos123@gmail.com>
2023-09-06 18:35:41 +0530

    Refactoring and added prettier

--- projects/asc-shared-libs/src/lib/interceptors/page-size.interceptor.ts
+++ projects/asc-shared-libs/src/lib/interceptors/page-size.interceptor.ts
@@ -11,30 +11,22 @@ import { Observable } from 'rxjs';
 export class PageSizeInterceptor implements HttpInterceptor {
   intercept(
     request: HttpRequest<any>,
-    next: HttpHandler
+    next: HttpHandler,
   ): Observable<HttpEvent<any>> {
     // Check if the request method is GET
     if (request.method === 'GET') {
-      // Check if there are path variables in the URL
-      if (!this.hasPathVariables(request.url)) {
-        // Add 'size' parameter with a large value (e.g., 2147483647) to retrieve all items
-        const updatedParams = request.params.set('size', '2147483647');
+      // Add 'size' parameter with a large value (e.g., 2147483647) to retrieve all items
+      const updatedParams = request.params.set('size', '2147483647');
 
-        // Clone the request and append the updated params
-        const updatedRequest = request.clone({
-          params: updatedParams,
-        });
+      // Clone the request and append the updated params
+      const updatedRequest = request.clone({
+        params: updatedParams,
+      });
 
-        return next.handle(updatedRequest);
-      }
+      return next.handle(updatedRequest);
     }
 
     // If not a GET request or if there are path variables, continue with the original request
     return next.handle(request);
-  }
-
-  private hasPathVariables(url: string): boolean {
-    // Check if the URL contains curly braces indicating path variables
-    return /\{.*?\}/.test(url);
   }
 }
